37215ae29d3e768041db931c4b99991b96190968
teco <230374@utxicotepec.edu.mx>
2024-09-24 14:13:23 -0600

    se agregó info al ejercicio 3 de la practica 5

--- js/Ejercicio_03.js
+++ js/Ejercicio_03.js
@@ -1,7 +1,7 @@
 // Repaso de objetos 
 
-const bg = "Linear-gradient(lldeg, rgb(2,0,36,1) 0%, rgba(9,9,121,1) 3%, rgb(0,212,255,1) 86%)"; 
-const style_console = `background: ${bg}; color:white; border-radius: 6px; padding: 4px; font-sixe: 1.0rem; font-weight: bold`
+const bg = "linear-gradient(11deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 33%, rgba(0,212,255,1) 86%)"; 
+const style_console = `background: ${bg}; color:white; border-radius: 6px; padding: 4px; font-size: 1.0rem; font-weight: bold`
 
 //Personalizacion de las salidas a consola
 console.warn("Practica 05: Repaso de Objetos en Java Script");
@@ -55,58 +55,74 @@ console.log(`Los datos del PRODUCTO son: \n
     //Ahora leemos el objeto completo
     console.table(Producto);
 
-    //Para acceder a los propiedades del objeto utilizamos un "." y el nombre de la propiedad a leer.
+    //Para acceder a los propiedades del objeto utilizamos un "." y el nombre de la propiedad a leer
     console.log("Accediendo a propiedades especificas del PRODUCTO")
-    console.log(`Nombre Completo del PRODUCTO: ${Producto.Nombre} ${Producto.Marca} ${Producto.Modelo}`)
+    console.log(`Nombre Completo del PRODUCTO ${Producto.Nombre} ${Producto.Marca} ${Producto.Modelo}`)
     console.log(`Precio: ${Producto.Precio}`)
     if(Producto.Disponibilidad == 0)
     console.log(`Estatus: Agotado`)
     else
     console.log(`Estatus: Disponible`)
 
-    
-    //Deestructuración de Objetos.
-        console.log("%c3.- Deestructuración de Objetos", style_console);
-        let Producto2 =
-        {
-            Clave : 316,
-            Nombre : "Lentes para sol",
-            Marca : "Oakley",
-            Modelo: "QNTM kato",
-            Precio : 6829.00,
-            Disponibilidad: true,
-            Stock: 5,
-            SKU : "009481D-0356",
-            Imagen: "../assets/products/sunglasses/KatoRed.png",
-            Barcode : 888392491626,
-            Categorias: ["Deportes", "lentes", "Hombre", "Accesorios"]
-        }
+    console.log("%c3.- Destructores de Objetos", style_console);
+    let Producto2 =
+    {
+        Clave : 316,
+        Nombre : "Lentes para Sol",
+        Marca : "Oarklev",
+        Modelo: "QNTM Kato",
+        Precio : 6829.00,
+        Disponibilidad: true,
+        Stock: 0,
+        SKU : "OO9481D-0356",
+        Imagen: "../asets/products/dunglasses/KatoRed.png",
+        Barcode : 88832491626,
+        Categorias: ["Deportes", "Tenis", "Juvenil"]
+    }
+    let Comprador = {
+        Clave : 316,
+        Nombre : "Diego ",
+        Apellidos : "Tecorralco",
+        Tipo : "Frecuente",
+        Correo : "dl7996409@gmail.com",
+        PaisOrigen :"Mexico",
+        SaldoActual: 14000.00,
+    }
+    let Pedido = {
+        Producto_Clave : 316,
+        Comprador_Clve : 3216,
+        Cantidad: 2,
+        Estatus : "Carrito de compra"
+    }
+    //En base a los 3 objetos necesitamos calcular el costo de la compra y si le alcanza con su saldo a favor
+    let {Precio :Producto_Precio2}= Producto2;
+    let {Cantidad : Pedido_Cantidad} = Pedido;
+    let {SaldoActual: Cliente_SaldoActual} = Comprador;
+    let Costo_Compra = Producto_Precio * Pedido_Cantidad;
 
-        let Comprador=
-        {
-            clave: 3216,
-            Nombre: "Diego",
-            Apellidos: "Tecorralco",
-            Tipo: "Frecuente",
-            Correo: "jaja@gmail.com",
-            PaisOrigen: "México",
-            SaldoActual: 14000.00
-        }
-        let Pedido =
-        {
-            Producto_Clave : 316,
-            Comprador_Clave: 3216,
-            Cantidad:2,
-            Estatus: "Carrito de Compra"
-        }
+    console.log(`El cliente ha agregado a su carrito de compras ${Pedido_Cantidad} unidades, con un costo total de : ${(Costo_Compra)}`);
+    if(Costo_Compra< Cliente_SaldoActual)
+        console.log("El cliente tiene saldo suficiente.")
 
-        //En base a los 3 productis necesitamos calcular el costo de la compra y si le alcanza con su saldo a favor
+    // Actualizar el valor de los objetos
+    console.log(`%c4.-Actualización de los valores de las propiedades de un Objeto`, style_console);
+    console.log(`El objeto actualmente tiene los isguientes valores`)
+    console.log(JSON.stringify(Producto2, null, 2));
+    // Convierte el objeto a una cadena para evitar problemas con la referencia
+    console.log(`Por cuestiones de inflacion el costo del producto ha cambiado y debe ser actualizado... de $6,829.00 a $6,925.50`)
 
-        let{Precio:Precio}= Producto2;
-        let {Cantidad:  Cantidad}= Pedido;
-        let {SaldoActual: SaldoActual} =Comprador;
-        let Costo_Compra= (Producto_Precio * Pedidio_Cantidad);
+    // Para podificar el valor de  un objeto basta con igualar el nuevo valor de la propiedad deseada 
+    Producto2.Precio=6915.50;
+    console.log(`Los nuevos valores del producto son: `)
+    console.log(Producto2);
 
-        console.log(`El cliente ha agregado a su carrito de compras ${Pedidio_Cantidad} unidades, con un costo de: $${Costo_Compra}`);
-        if(Costo_Compra<Cliente_SaldoActual)
-            console.log("El Cliente tiene saldo suficiente.");
+    //¿Puedo cambiar no solo el valor si no el tipo de dato de un Objeto en JavaScript?
+    console.log(`--------------------`)
+    console.log(`El objeto actualmente tiene los siguientes valores`)
+    var tipoDisponibilidad = typeof(Producto.Disponibilidad)
+    console.log(`El tipo de dato de la disponibilidad es: ${tipoDisponibilidad}`)
+    console.log(JSON.stringify(Producto2,null, 2));
+    Producto2.Disponibilidad="Si";
+    let nuevoTipoDisponibilidad = typeof(Producto2.Disponibilidad)
+    console.log(Producto2);
+    console.log(`El nuevo tipo de dato de la disponibilidad es: ${nuevoTipoDisponibilidad}`)
